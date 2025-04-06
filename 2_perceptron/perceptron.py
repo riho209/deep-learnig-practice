4ffed2226cddbd39692d18815c8e1daa3b1bf424
LAPTOP-DU356672\riho2 <rihojianbu@gmail.com>
2025-04-06 23:24:17 +0900

    np配列とバイアスによる評価に変更

--- 2_perceptron/perceptron.py
+++ 2_perceptron/perceptron.py
@@ -1,9 +1,12 @@
 import numpy as np
 
 def AND(x1,x2):
-    w1,w2,theta=0.5,0.5,0.7
-    num=w1*x1+x2*w2
-    if num<=theta:
+    #重みとバイアス
+    x=np.array([x1,x2])
+    w=np.array([0.5,0.5])
+    b=-0.7
+    num=np.sum(w*x)+b
+    if num<=0:
         y=0
     else:
         y=1
